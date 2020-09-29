6067bcc69af14efff212664e8f8e20b2e8d8c553
wasim0315 <wasimkhan03150@gmail.com>
2020-09-29 09:41:53 +0530

    Just after first send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -69,7 +69,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#compose-form').style.display = 'none';
+  
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
